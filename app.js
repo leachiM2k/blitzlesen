2cf1c1aa96c0b2fb54c8ac3e4425c9a30fe335b2
Michael Rotmanov <leachiM2k@leachiM2k.de>
2021-04-29 16:04:31 +0200

    added more options, transformed to ES6, updated packages

--- app.js
+++ app.js
@@ -1,77 +1,58 @@
-var path = require('path');
-var fs = require('fs');
-var events = require('events');
-var util = require('util');
-var http = require('http');
-var express = require('express');
-var logger = require('morgan');
-var methodOverride = require('method-override');
-var errorHandler = require('errorhandler');
-var cookieParser = require('cookie-parser');
-var notemplate = require('express-notemplate');
-var GoogleSpreadsheet = require("google-spreadsheet");
-var shuffle = require('knuth-shuffle').knuthShuffle;
+const http = require('http');
+const express = require('express');
+const errorHandler = require('errorhandler');
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 
-var sheetKey = '1bAPNRFjr83ghmxtIyldtlXJZrNYoQcw1KgsDprbWowM';
-
-var wordlists = {};
-
-function loadWords(handler)
-{
-    var workbook = new GoogleSpreadsheet(sheetKey);
-    workbook.getInfo(function (err, info) {
-        if (err) {
-            handler(err);
-            return;
-        }
-        var sheets = {};
-        function getSheet(sheetNo) {
-            var sheet = info.worksheets[sheetNo];
-            sheet.getRows(function (err, rows) {
-                if (err) {
-                    handler(err);
-                    return;
-                }
-                sheets[sheet.title] = rows.map(function (row) { return row.title; });
-                if (sheetNo == info.worksheets.length - 1) {
-                    handler(null, sheets);
-                } else {
-                    getSheet(sheetNo + 1);
-                }
-            });
-        }
-        getSheet(0);
-    });
-}
-
-var app = express();
+const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
-app.use(methodOverride());
-app.use(cookieParser('blikdiblu'));
 app.set('statics', process.cwd() + '/public');
-app.engine('html', notemplate.__express);
 app.set('view engine', 'html');
 app.use(express.static(app.get('statics')));
 app.use(errorHandler());
 
-var server = http.createServer(app);
-app.get('/', function (req, res) {
-    res.redirect('/blitzlesen.html');
-});
+const server = http.createServer(app);
 
-app.get('/wortlisten', function (req, res) {
-    loadWords(function (err, data) {
-        if (err) {
-            res.send(500, err);
+if (!process.env.SHEET_KEY) {
+    console.error('Environment varialbe SHEET_KEY is missing.');
+    process.exit(1);
+}
+
+if (!process.env.GOOGLE_API_KEY) {
+    console.error('Environment varialbe GOOGLE_API_KEY is missing.');
+    process.exit(1);
+}
+
+const sheetKey = process.env.SHEET_KEY;
+const apiKey = process.env.GOOGLE_API_KEY;
+
+const loadWords = async handler => {
+    const workbook = new GoogleSpreadsheet(sheetKey);
+    workbook.useApiKey(apiKey);
+    await workbook.loadInfo(); // loads document properties and worksheets
+    const sheets = {};
+
+    async function getSheet(sheetNo) {
+        const sheet = workbook.sheetsByIndex[sheetNo];
+        const rows = await sheet.getRows();
+        sheets[sheet.title] = rows.map(row => row.title);
+        if (sheetNo === workbook.sheetCount - 1) {
+            return sheets;
         } else {
-            res.json(data);
+            return await getSheet(sheetNo + 1);
         }
-    });
+    }
+
+    return await getSheet(0);
+};
+
+app.get('/wordlists', async (req, res) => {
+    try {
+        res.json(await loadWords());
+    } catch (err) {
+        res.send(500, err);
+    }
 });
 
-server.listen(app.get('port'), function() {
-    console.log("Express server listening on port " + app.get('port'));
-});
+server.listen(app.get('port'), () => console.log("Express server listening on port " + app.get('port')));
 
